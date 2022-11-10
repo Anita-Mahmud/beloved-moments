a6b93ef8073794e0541954391b20dc53936fd7fb
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-11-10 23:04:23 +0600

    google login added

--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
 import reg from '../../images/camera.jpg'
 const Register = () => {
-    
+   
+    const {signUp,updateUserProfile} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+    //signUp
+    const handleSignUp = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const displayName = form.username.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        //signUp
+        signUp(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            navigate(from, {replace: true});
+            handleUserProfile(displayName,photoURL);
+            form.reset();
+           
+        })
+        .catch(error => {
+          console.error(error);
+         
+      });
+        //  profile
+        const handleUserProfile = (displayName,photoURL)=>{
+          const profile = {displayName,photoURL}
+          updateUserProfile (profile)
+          .then(()=>{})
+          .catch(error=>console.error(error))
+        }
+
+    }
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 '>
             <Helmet>
@@ -15,7 +51,7 @@ const Register = () => {
         </div>
         <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100 mx-auto mb-2">
 <h1 className="text-2xl font-bold text-center">Register</h1>
-<form  novalidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+<form onSubmit={handleSignUp} novalidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
     <div className="space-y-1 text-sm">
         <label for="username" className="block dark:text-gray-400">Full Name</label>
         <input type="text" name="username" id="username" placeholder="Full Name" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
@@ -33,7 +69,7 @@ const Register = () => {
         <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
         
     </div>
-    <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400 bg-pink-500 text-white">Sign up</button>
+    <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400 bg-pink-500 text-white" type='submit'>Sign up</button>
 </form>
 
 <p className="text-xs text-center sm:px-6 dark:text-gray-400">Already have an account?
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createContext } from "react";
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from '../firebase/firebase.config';
 export const AuthContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     // google
     const googleAuthProvider = (provider)=>{
-        signInWithPopup(auth,provider);
+        setLoading(true);
+       return signInWithPopup(auth,provider);
+    }
+    // register
+    const signUp = (email,password)=>{
+        setLoading(true);
+      return  createUserWithEmailAndPassword(auth,email,password);
+    }
+    // user profile
+    const updateUserProfile =(profile)=>{
+        return updateProfile(auth.currentUser,profile)
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+         setUser(currentUser);
+        setLoading(false);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+
+    }, [])
+    //logout
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
     }
     const authInfo = {
         googleAuthProvider,
+        signUp,
+        updateUserProfile,
+        logOut,
+        loading,
+        user
     };
     return (
         <AuthContext.Provider value={authInfo}>
